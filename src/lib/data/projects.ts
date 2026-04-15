84e1e4e680226f053ded17421f91edebadb9544a
Adrian Kulik <adriankulik93@gmail.com>
2026-04-16 00:15:38 +0200

    Add mock projects

---
+++ src/lib/data/projects.ts
@@ -0,0 +1,49 @@
+export const projects: {
+	title: string;
+	tagline: string;
+	tech: string[];
+	imageUrl: string;
+	imageUrl2?: string;
+	imageUrl3?: string;
+	tagColor: string;
+}[] = [
+	{
+		title: 'Nova Banking Shell',
+		tagline:
+			'Reimagining financial data through the lens of clarity and high-performance visualizations.',
+		tech: ['Fintech', 'React'],
+		imageUrl:
+			'https://lh3.googleusercontent.com/aida-public/AB6AXuD43O14RjuJa-XPUVzdHhdDLkQNsN56B3si6b7LmDY9E7TLuuj4ml3rLAztsa4TKTNuD3imwyfQwW51vnNl-wcP6ZGcrXLzFg5jPS94BTQ2itFVBXl9CDEjuRQ8Qj0NhUmF609YalXZ40J_J8ZJ4EyL8zVfjyjYCIcsP1PGAStz2smwuMtVyQ1oeW8f_vImb-7H1CwGhG0GoY2bCV2hOCwOfBxVXkILsJffFClYz72xj7sdmE6yEms42bsWVsZnDnT9kp00jM5LqJ4',
+		imageUrl2:
+			'https://lh3.googleusercontent.com/aida-public/AB6AXuD43O14RjuJa-XPUVzdHhdDLkQNsN56B3si6b7LmDY9E7TLuuj4ml3rLAztsa4TKTNuD3imwyfQwW51vnNl-wcP6ZGcrXLzFg5jPS94BTQ2itFVBXl9CDEjuRQ8Qj0NhUmF609YalXZ40J_J8ZJ4EyL8zVfjyjYCIcsP1PGAStz2smwuMtVyQ1oeW8f_vImb-7H1CwGhG0GoY2bCV2hOCwOfBxVXkILsJffFClYz72xj7sdmE6yEms42bsWVsZnDnT9kp00jM5LqJ4',
+		imageUrl3:
+			'https://lh3.googleusercontent.com/aida-public/AB6AXuD43O14RjuJa-XPUVzdHhdDLkQNsN56B3si6b7LmDY9E7TLuuj4ml3rLAztsa4TKTNuD3imwyfQwW51vnNl-wcP6ZGcrXLzFg5jPS94BTQ2itFVBXl9CDEjuRQ8Qj0NhUmF609YalXZ40J_J8ZJ4EyL8zVfjyjYCIcsP1PGAStz2smwuMtVyQ1oeW8f_vImb-7H1CwGhG0GoY2bCV2hOCwOfBxVXkILsJffFClYz72xj7sdmE6yEms42bsWVsZnDnT9kp00jM5LqJ4',
+		tagColor: 'text-on-surface-variant'
+	},
+	{
+		title: 'Atelier AR',
+		tagline: 'Spatial computing app for interior designers.',
+		tech: ['Mobile'],
+		imageUrl:
+			'https://lh3.googleusercontent.com/aida-public/AB6AXuAWmIO7ryqqfYiHaMqxZZkgx0bokWfG63q3wSCsd2kStUkQzHHfEkCTJ-VYWe_tZCeOaNnTjlOXmXcSDHH9t8r0QhkokJwoTszeT0E62-Z-eam7vhtZthz9RPFqliD_0H274G_rkAhz9MI9URGw7bKREb0kYewfSKEcjTdEzgOJ2HHj0uZj_OT2gU-Q51QDudYSHnJCzHeA7AorcTtUO6NvCvkUjQtVRR7mpgzjoO3ye2kIgjK5sZitbdqnoE9I9MwaB6CoZbb9hkI',
+		imageUrl2:
+			'https://lh3.googleusercontent.com/aida-public/AB6AXuAWmIO7ryqqfYiHaMqxZZkgx0bokWfG63q3wSCsd2kStUkQzHHfEkCTJ-VYWe_tZCeOaNnTjlOXmXcSDHH9t8r0QhkokJwoTszeT0E62-Z-eam7vhtZthz9RPFqliD_0H274G_rkAhz9MI9URGw7bKREb0kYewfSKEcjTdEzgOJ2HHj0uZj_OT2gU-Q51QDudYSHnJCzHeA7AorcTtUO6NvCvkUjQtVRR7mpgzjoO3ye2kIgjK5sZitbdqnoE9I9MwaB6CoZbb9hkI',
+		tagColor: 'text-on-surface-variant'
+	},
+	{
+		title: 'Nexus IDE Theme',
+		tagline: 'A productivity-first color system for long nights of coding.',
+		tech: ['DevTools'],
+		imageUrl:
+			'https://lh3.googleusercontent.com/aida-public/AB6AXuD4etaZr_BJRMtZEzBl3tlkTqxwpz_xtH_5mr11Yo09GQTx9OkV7xZyZZ5gBkJHxxJBb7_7J5GK_aRHL_iTKau8CcNilMFZW5xUQYwIsvz0iDPThrbJITJCTZG6LLFyh98mOhAWFhndpBTkOqDVn7lRCyLXBym-RF12FXe_bX7cmEfC4V9r75NNbxnV-XK3OS07iFp5vfd3LtmXPL27XjRGkS6lhlKBtiPbnij57CIeNPmvy6tKbC0OHodkfEFuGL9UL_ScifrMdCo',
+		tagColor: 'text-on-surface-variant'
+	},
+	{
+		title: 'Lune Editorial',
+		tagline: 'Where high-fashion commerce meets immersive storytelling experiences.',
+		tech: ['E-Commerce'],
+		imageUrl:
+			'https://lh3.googleusercontent.com/aida-public/AB6AXuAKr066Kt63HvLAKNm9PUxJqII7cLsApkGy87R9_oV5WI7U921Sg1Y9KWdMqaV8eNBPmOWfr6dLWlOWUtHHjc02DF7G93p8-bfla24copVqIABkL32KzOlEerEH87tPfOO1NG0ClDTOTKL5aiFm5noAqzjTpfVhon1CGJODSYbdeCfjWCADWHRcLeZYX8ETttX3Sv9yhxmY_3qgtCKvPsR4FNiVeVToiXWGbr9x0Af9gsZHJJrOvTUdwjpY55BYamhuC9SXJMZfiZ8',
+		tagColor: 'text-on-surface-variant'
+	}
+];
